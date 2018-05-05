79686a27559ac753c60362a82fd10127034160d5
Neil Ricci <iccir919@gmail.com>
2018-05-05 13:05:23 -0700

    Remove functions from my answers, save 'em for next chapter

--- chapt2.js
+++ chapt2.js
@@ -1,53 +1,49 @@
-function loopingATriangle(){
-    let string = "#";
-    while(string.length <= 7){
-        console.log(string);
-        string += "#";
-    }
+
+let string = "#";
+while(string.length <= 7){
+    console.log(string);
+    string += "#";
 }
 
-function fizzBuzz(){
-    for(let i = 1; i <= 100; i++){
-        if((i%3 === 0) || (i%5 === 0)){
-            let output = "";
-            if(i%3 === 0){
-                output += "Fizz";
-            }
-            if(i%5 === 0){
-                output += "Buzz";
-            }
-            console.log(output);
-        } else {
-            console.log(i);
+for(let i = 1; i <= 100; i++){
+    if((i%3 === 0) || (i%5 === 0)){
+        let output = "";
+        if(i%3 === 0){
+            output += "Fizz";
         }
-
+        if(i%5 === 0){
+            output += "Buzz";
+        }
+        console.log(output);
+    } else {
+        console.log(i);
     }
+
 }
 
-function chessBoard(){
-    let size = 12;
-    let string = "";
-    
-    for(let i = 0; i < size; i++){
-        for(let j = 0; j < size; j++){
-            if(i%2 === 0){
-                if(j%2 === 0){
-                    string += " ";
-                } else {
-                    string += "#";
-                }
+let size = 12;
+let string = "";
+
+for(let i = 0; i < size; i++){
+    for(let j = 0; j < size; j++){
+        if(i%2 === 0){
+            if(j%2 === 0){
+                string += " ";
             } else {
-                if(j%2 === 0){
-                    string += "#";
-                } else {
-                    string += " ";
-                }
+                string += "#";
+            }
+        } else {
+            if(j%2 === 0){
+                string += "#";
+            } else {
+                string += " ";
             }
         }
-        string += "\n"
     }
-    return string;
+    string += "\n"
 }
+console.log(string);
+
 /*
 //////////////// BOOK SOLUTIONS ////////////////////
 
