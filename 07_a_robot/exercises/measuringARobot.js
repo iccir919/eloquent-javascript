fb32088d68f80dbfa210c5d16c0307acd92b7765
Neil Ricci <iccir919@gmail.com>
2020-03-29 15:37:50 -0700

    Complete some of Chapter 7 exercises

--- 07_a_robot/exercises/measuringARobot.js
+++ 07_a_robot/exercises/measuringARobot.js
@@ -1,3 +1,5 @@
+require("../chapter/07_robot");
+
 /*
     It’s hard to objectively compare robots by just letting them solve a few scenarios. 
     Maybe one robot just happened to get easier tasks or the kind of tasks that it 
@@ -12,7 +14,28 @@
 */
 
 function compareRobots(robot1, memory1, robot2, memory2) {
-    // Your code here
+    let robot1Total = 0;
+    let robot2Total = 0;
+
+    for (let i = 0; i < 100; i++) {
+        let task = VillageState.random();
+        robot1Total += countSteps(task, robot1, memory1);
+        robot2Total += countSteps(task, robot2, memory2);
+    }
+
+    console.log(`Robot 1 averaged ${robot1Total / 100} turns per task`);
+    console.log(`Robot 2 averaged ${robot2Total / 100} turns per task`);
+}
+
+function countSteps(state, robot, memory) {
+    for (let turn = 0;; turn++) {
+        if (state.parcels.length == 0) {
+            return turn;
+        }
+        let action = robot(state, memory);
+        state = state.move(action.direction);
+        memory = action.memory;
+    }
 }
 
 compareRobots(routeRobot, [], goalOrientedRobot, []);
--- 07_a_robot/exercises/robotEfficiency.js
+++ 07_a_robot/exercises/robotEfficiency.js
@@ -7,6 +7,35 @@
     function to verify whether you improved the robot.
 */
 
-// Your code here
+let memory = [];
+
+function yourRobot({place, parcels}, route) {
+    if (route.length === 0) {
+      let pickups = parcels
+          .filter(parcel => parcel.place !== place)
+          .map((parcel) => findRoute(roadGraph, place, parcel.place));
+      let dropoffs = parcels
+          .filter(parcel => parcel.place === place)
+          .map((parcel) => findRoute(roadGraph, place, parcel.address));
+      
+      let shortestPickupRoute, shortestDropoffRoute;
+      if (pickups.length) {
+          shortestPickupRoute = pickups.reduce((accum, cur) => accum.length > cur.length ? cur : accum);
+      }
+      if (dropoffs.length) {
+          shortestDropoffRoute = dropoffs.reduce((accum, cur) => accum.length > cur.length ? cur : accum);
+      }
+      
+      if (shortestPickupRoute && shortestDropoffRoute) {
+          route = shortestPickupRoute.length <= shortestDropoffRoute.length ? shortestPickupRoute : shortestDropoffRoute;
+      } else if (!shortestPickupRoute) {
+          route = shortestDropoffRoute;
+      } else {
+          route = shortestPickupRoute;
+      }
+    }
+    return {direction: route[0], memory: route.slice(1)};
+  }
+
 
 runRobotAnimation(VillageState.random(), yourRobot, memory);
