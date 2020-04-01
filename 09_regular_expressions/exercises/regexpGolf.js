32fcbe619c8aad3017fbafc7eaced2dfba1aff9c
Neil Ricci <iccir919@gmail.com>
2020-03-31 23:56:08 -0700

    Complete one exercise from Chapter 9

--- 09_regular_expressions/exercises/regexpGolf.js
+++ 09_regular_expressions/exercises/regexpGolf.js
@@ -26,42 +26,43 @@
 
 // Fill in the regular expressions
 
-verify(/.../,
+verify(/ca[rt]/,
     ["my car", "bad cats"],
     ["camper", "high art"]);
 
-verify(/.../,
+verify(/pr?op/,
     ["pop culture", "mad props"],
     ["plop", "prrrop"]);
 
-verify(/.../,
+verify(/ferr[ari|y|et]/,
     ["ferret", "ferry", "ferrari"],
     ["ferrum", "transfer A"]);
 
-verify(/.../,
+verify(/\w*ious\b/,
     ["how delicious", "spacious room"],
     ["ruinous", "consciousness"]);
 
-verify(/.../,
+verify(/\s[.;:,]/,
     ["bad punctuation ."],
     ["escape the period"]);
 
-verify(/.../,
+verify(/\b\w{7,}\b/,
     ["hottentottententen"],
     ["no", "hotten totten tenten"]);
 
-verify(/.../,
+verify(/\b[a-d|f-z]+\b/i,
     ["red platypus", "wobbling nest"],
     ["earth bed", "learning ape", "BEET"]);
 
 
 function verify(regexp, yes, no) {
-// Ignore unfinished exercises
-if (regexp.source == "...") return;
-for (let str of yes) if (!regexp.test(str)) {
- console.log(`Failure to match '${str}'`);
-}
-for (let str of no) if (regexp.test(str)) {
- console.log(`Unexpected match for '${str}'`);
-}
+    // Ignore unfinished exercises
+    if (regexp.source == "...") return;
+    for (let str of yes) if (!regexp.test(str)) {
+        console.log(`Failure to match '${str}'`);
+    }
+    for (let str of no) if (regexp.test(str)) {
+        console.log(`Unexpected match for '${str}'`);
+
+    }
 }
